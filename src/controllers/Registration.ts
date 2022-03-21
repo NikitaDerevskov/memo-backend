754b1f00aac4f5796696a3d710f53a1cf97c2b1a
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-03-22 01:15:15 +0300

    Use bcrypt and create salt, hash

--- src/controllers/Registration.ts
+++ src/controllers/Registration.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 
-const handleRegistration = (req: express.Request, res: express.Response): void => {
+const handleRegistration = async (req: express.Request, res: express.Response) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    res.status(400).json('Incorrect form submission');
+    return res.status(400).json('Incorrect form submission');
   }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  return res.send(hash);
+
+  /*
+  * TODO create hash +
+  * TODO Check if this user exist (send message User with this email already exist.)
+  *  If no - create in database
+  * TODO insert hash to database
+  * */
 };
 
 export default handleRegistration;

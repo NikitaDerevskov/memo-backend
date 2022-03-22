386f99b0fe416147ba95a90f3f3a5719d708e76b
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-03-23 00:39:32 +0300

    Prevent recreating user, add register user

--- src/controllers/Registration.ts
+++ src/controllers/Registration.ts
@@ -1,21 +1,47 @@
-import express from 'express';
 import bcrypt from 'bcryptjs';
+import { Knex } from 'knex';
+import express from 'express';
 
-const handleRegistration = async (req: express.Request, res: express.Response, db) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
+const handleRegistration = async (req: express.Request, res: express.Response, db: Knex<any, unknown[]>) => {
+  const { email, password, name } = req.body;
+  if (!email || !password || !name) {
     return res.status(400).json('Incorrect form submission');
+  }
+
+  const userWithThisEmail = await db('users')
+    .where('email', email)
+    .catch(() => {
+      res.send('Error in get user with this email');
+      return [];
+    });
+
+  const userExist = userWithThisEmail[0];
+
+  if (userExist) {
+    return res.status(400).json('User with current email already exist');
   }
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
-  console.log(db)
-  return res.send(hash);
+
+  await db('users').insert({
+    email, name, entries: 0, joined: new Date().toISOString(),
+  });
+
+  // // name, email, entries, joined) values ('a', 'a@a.com', 5, '2018-01-01');
+  // const b = await db('users')
+  //   .where('email', email);
+  // res.send(b);
+  // TODO maybe save hash of password in user ?
+
+  // return res.send(hash);
+  // const test2 = await db('users').where('email', email);
+  // console.log('hey', test, test2);
 
   /*
   * TODO create hash +
-  * TODO Check if this user exist (send message User with this email already exist.)
-  *  If no - create in database
+  * TODO Check if this user exist (send message User with this email already exist.) +
+  *  If no - create in database +
   * TODO insert hash to database
   * */
 };

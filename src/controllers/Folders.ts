75f61cbb9a071d2625017c56eae93c8563bd3ff0
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-03-30 01:29:54 +0300

    Add abstract functions to folders and cards

---
+++ src/controllers/Folders.ts
@@ -0,0 +1,9 @@
+export const createFolder = () => 0;
+
+export const renameFolder = () => 0;
+
+export const getFolders = () => 0;
+
+export const getFolder = () => 0;
+
+export const deleteFolder = () => 0;

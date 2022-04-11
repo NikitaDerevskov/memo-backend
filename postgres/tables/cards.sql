b1970631c971652686bdfc556e82177b7e7f1b70
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-04-12 00:15:04 +0300

    Remove created from cards

--- postgres/tables/cards.sql
+++ postgres/tables/cards.sql
@@ -4,7 +4,6 @@ CREATE TABLE cards (
     id serial PRIMARY KEY,
     title VARCHAR(100),
     content VARCHAR(5000),
-    created TIMESTAMP NOT NULL,
     last_modified TIMESTAMP NOT NULL,
     folder_id integer not null references folders(id)
 );

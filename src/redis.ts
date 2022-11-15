43deef9a32818d0de7929f42c184ba980e22d4d1
Nikita Derevskov <n.derevskov@examus.com>
2022-11-15 08:31:48 +0300

    root: redis.ts: log error

--- src/redis.ts
+++ src/redis.ts
@@ -3,4 +3,8 @@ import { createClient } from 'redis';
 const redisClient = createClient({ url: process.env.REDIS_URI });
 redisClient.connect().then(() => console.log('Redis connected'));
 
+redisClient.on('error', (e) => {
+  console.error('Redis error', e);
+});
+
 export default redisClient;
